ccc483aa2059c88ebd6563e72970546390f617b4
Ion <ion.ciuchitu@gmail.com>
2023-05-18 10:26:21 +0300

    Ion_Lab7_branch

--- src/lab7/EndWorkDay.java
+++ src/lab7/EndWorkDay.java
@@ -1,4 +1,20 @@
 package lab7;
 
-public class EndWorkDay {
+
+public class EndWorkDay extends Thread{
+    public static int end;
+    private boolean workDayEnded = false;
+    @Override
+    public void run(){
+        try {
+            sleep(20000);
+            workDayEnded = true;
+        } catch (InterruptedException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public boolean isWorkDayEnded() {
+        return workDayEnded;
+    }
 }
